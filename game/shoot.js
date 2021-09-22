721abd3856d071287367e704636ae761a9091620
zakaria.ben-allal <zakaria.ben-allal@epita.fr>
2021-09-22 20:57:58 +0200

    WIP

--- game/shoot.js
+++ game/shoot.js
@@ -60,7 +60,8 @@ function player_collision()
     //collision between player and walls
     var x = player1.graphic.position.x + WIDTH / 2;
     var y = player1.graphic.position.y + HEIGHT / 2;
-
+    if ( x < 0 )
+        player1.graphic.position.x -= x;
     if ( x > WIDTH )
         player1.graphic.position.x -= x - WIDTH;
     if ( y < 0 )
@@ -82,7 +83,7 @@ function player_falling()
 
     for (var i = 0; i < length; i++) {
         element = noGround[i];
-
+        if (element === undefined) break;
         var tileX = (element[0]) | 0;
         var tileY = (element[1]) | 0;
         var mtileX = (element[0] + sizeOfTileX) | 0;
